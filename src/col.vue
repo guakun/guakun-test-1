0f48be28d6ed268102dc8a9eca061aa916083bd7
unknown <2306831967@qq.com>
2018-07-31 23:31:32 +0800

    稍微重构了一下

--- src/col.vue
+++ src/col.vue
@@ -34,16 +34,25 @@ export default {
       gutter: 0
     };
   },
+  methods: {
+    createClasses(obj, str = "") {
+      let o = [];
+      if (!obj) { return []; }
+      if (obj.span) { o.push(`col-${str}${obj.span}`); }
+      if (obj.offset) { o.push(`offset-${str}${obj.offset}`); }
+      return o;
+    }
+  },
   computed: {
     colClass() {
       let { span, offset, phone, ipad, narrowPc, pc, widePc } = this;
+      let createClasses = this.createClasses
       return [
-        span && `col-${span}`,
-        offset && `offset-${offset}`,
-        ...(ipad ? [`col-ipad-${ipad.span}`] : []),
-        ...(narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-        ...(pc ? [`col-pc-${pc.span}`] : []),
-        ...(widePc ? [`col-wide-pc-${widePc.span}`] : [])
+        ...createClasses({ span, offset }),
+        ...createClasses(ipad, "ipad-"),
+        ...createClasses(narrowPc, "narrow-pc-"),
+        ...createClasses(pc, "pc-"),
+        ...createClasses(widePc, "wide-pc-")
       ];
     },
     colStyle() {
